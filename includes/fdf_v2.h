3069d2d609fac72990e422973d0b081b27034786
Antoine MULIN <antoine.mulin@gmail.com>
2015-04-07 16:09:50 +0200

    Norme OK, implemented input file print

--- includes/fdf_v2.h
+++ includes/fdf_v2.h
@@ -6,27 +6,14 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/17 11:06:47 by amulin            #+#    #+#             */
-/*   Updated: 2015/02/27 14:11:00 by amulin           ###   ########.fr       */
+/*   Updated: 2015/04/07 16:08:32 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FDF_H
-# define FDF_H
+#ifndef FDF_V2_H
+# define FDF_V2_H
 
-//	# include <fcntl.h>
+# include <fcntl.h>
 # include "libft.h"
-//	# include <mlx.h>
-//	# include <stdio.h>
-
-typedef	struct	s_params
-{
-//	void	*id;
-//	void	*win;
-//	int		***map;
-
-//	int		posx;
-//	int		posy;
-
-}				t_params;
 
 #endif
--- sources/main_v2.c
+++ sources/main_v2.c
@@ -6,7 +6,7 @@
 /*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/17 11:07:06 by amulin            #+#    #+#             */
-/*   Updated: 2015/02/27 14:36:08 by amulin           ###   ########.fr       */
+/*   Updated: 2015/04/07 16:07:08 by amulin           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,22 +20,34 @@ void	print_man(void)
 	ft_putendl("       Use { to zoom in, and } to zoom out");
 }
 
-int		**convert_map(char *filename)
+int		my_open(char *filename)
 {
-//	Check filename is a file
-//	Check map is valid
-//	Copy map
+	return (open(filename, O_RDONLY));
+}
+
+int		tbd_print_input(char *filename)
+{
+	int		fd;
+	char	**line;
+
+	fd = my_open(filename);
+	line = (char**)malloc(sizeof(char*));
+	if (!line)
+		return (-1);
+	if (fd < 0)
+		return (-1);
+	while (get_next_line(fd, line) == 1)
+		ft_putendl(*line);
+	ft_putendl(*line);
+	return (0);
 }
 
 int		main(int argc, char **argv)
 {
-	int	**map;
-
 	if (argc > 1 && argc < 4)
 	{
-		map = convert_map(argv[1]);
-//	Parse
-//	Draw
+		ft_putendl("Printing input map...");
+		tbd_print_input(argv[1]);
 	}
 	else
 		print_man();
