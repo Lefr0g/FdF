ac6fb43fa18b1992e54a22e64d3b4a75ae1807e2
Lefr0g <amulin@student.42.fr>
2015-04-07 15:38:51 +0200

    Commiting new header files

---
+++ includes/fdf_v2.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fdf_v2.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amulin <amulin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/02/17 11:06:47 by amulin            #+#    #+#             */
+/*   Updated: 2015/02/27 14:11:00 by amulin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FDF_H
+# define FDF_H
+
+//	# include <fcntl.h>
+# include "libft.h"
+//	# include <mlx.h>
+//	# include <stdio.h>
+
+typedef	struct	s_params
+{
+//	void	*id;
+//	void	*win;
+//	int		***map;
+
+//	int		posx;
+//	int		posy;
+
+}				t_params;
+
+#endif
